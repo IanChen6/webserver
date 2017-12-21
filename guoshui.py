55fd193e55d268e8770344dc4d998d99e2e59072
IanChen6 <626614767@qq.com>
2017-12-21 19:01:28 +0800

    save png alter

--- guoshui.py
+++ guoshui.py
@@ -103,30 +103,8 @@ class guoshui(object):
         return h
 
     def save_png(self, browser, path):
-        browser.execute_script("""
-        (function () {
-        var y = 0;
-        var step = 100;
-        window.scroll(0, 0);
-        function f() {
-          if (y < document.body.scrollHeight) {
-            y += step;
-            window.scroll(0, y);
-            setTimeout(f, 50);
-          } else {
-            window.scroll(0, 0);
-            document.title += "scroll-done";
-          }
-        }
-        setTimeout(f, 1000);
-      })();
-    """)
-        for i in range(30):
-            if "scroll-done" in browser.title:
-                break
-            time.sleep(1)
-        # browser.save_screenshot(path)
-        browser.get_screenshot_as_file(path)
+        browser.save_screenshot(path)
+        # browser.get_screenshot_as_file(path)
         img=self.upload_img(path)
         return img
 
